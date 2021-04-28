bee83beffca3f252402eb70ad35803ec7e8f4797
lila <lsmith2@olin.edu>
2021-04-27 20:58:37 -0400

    Added MVC files.

---
+++ snake_model.py
@@ -0,0 +1,36 @@
+class GameBoard:
+    """
+    Attributes:
+_board: a 2d list containing the one of objects Apple, Blank, Border, SnakeTail, or SnakeHead for each square of the board
+_snake_direction: current direction of motion
+_snake_length: current snake length 
+_dimensions: the side length of the board
+_timestep: time for snake to move one block
+Maybe move to main.py
+    """
+    def __init__(self):
+        pass
+
+    def change_direction(self, direction):
+        pass
+
+    def maintain_velocity(self):
+        pass
+
+    def increase_length(self):
+        pass
+    
+    def check_next_square(self):
+        pass
+    
+    def spawn_apple(self):
+        pass
+    
+    def game_over(self):
+        pass
+    
+    def get_square(self):
+        pass
+    
+    def __repr__(self):
+        pass
