f41b9e6a7c63579758a312697760ee724beb1cc5
Alex Matsoukas <amatsoukas@olin.edu>
2021-04-27 22:20:28 -0400

    Outline view and controller functions/methods. Create PyGame view.

---
+++ snake_controller.py
@@ -0,0 +1,23 @@
+class SnakePlayer:
+    """
+    
+    """
+    
+    def __init__(self, board_instance):
+        """
+        
+        """
+        self._board = board_instance
+        
+    @property
+    def board(self):
+        """
+        
+        """
+        return self._board
+    
+    def get_input(self):
+        """
+        
+        """
+        pass
---
+++ snake_view.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+import pygame
+
+class SnakeView(ABC):
+    """
+    
+    """
+    
+    def __init__(self, board_instance):
+        """
+        
+        """
+        self._board = board_instance
+        
+    @property
+    def board(self):
+        """
+
+        """
+        return self._board
+    
+    @abstractmethod
+    def draw(self):
+        """
+        
+        """
+        pass
+
+    
+class TextView(SnakeView):
+    """
+    
+    """
+    
+    def draw(self):
+        """
+        
+        """
+        pass
+    
+    
+class PygameView(SnakeView):
+    """
+    
+    """
+    
+    def draw(self):
+        """
+        
+        """
+        pygame.init()
+        
+        scale_factor = 30
+        screen_width = self.board.size * scale_factor
+        screen_height = self.board.size * scale_factor
+        
+        screen = pygame.display.set_mode((screen_width, screen_height))
+        
+        
+        for row_index, row in enumerate(self.board.board_array):
+            for col_index, item in enumerate(row):
+                pygame.draw.rect(screen, item, (row_index * 10, col_index * 10, scale_factor, scale_factor))
+        
+        pygame.display.flip()
+        
+        
