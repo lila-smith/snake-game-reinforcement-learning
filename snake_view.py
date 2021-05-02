2c2a20bd744a1d09da8600d4ee6ebe77eea6b57a
Alex Matsoukas <amatsoukas@olin.edu>
2021-05-02 12:12:24 -0400

    Change drawing so that the pygame screen is only set up once in the init

--- snake_view.py
+++ snake_view.py
@@ -44,21 +44,40 @@ class PygameView(SnakeView):
     
     """        
     
+    def __init__(self, board_instance):
+        """
+        
+        """
+        super().__init__(board_instance)
+        
+        self._scale_factor = 30
+        self._screen_size = self.board.size * self._scale_factor, self.board.size * self._scale_factor
+        self._screen = pygame.display.set_mode(self._screen_size)
+    
+    @property
+    def scale_factor(self):
+        """
+        
+        """
+        return self._scale_factor
+    
+    @property
+    def screen(self):
+        """
+        
+        """
+        return self._screen
+    
     def draw(self):
         """
         
         """   
         pygame.init()
         
-        scale_factor = 30
-        screen_width = self.board.size * scale_factor
-        screen_height = self.board.size * scale_factor
-        
-        screen = pygame.display.set_mode((screen_width, screen_height))
         
         for row_index, row in enumerate(self.board.board_array):
             for col_index, item in enumerate(row):
-                pygame.draw.rect(screen, item.color, (col_index * scale_factor, row_index * scale_factor, scale_factor, scale_factor))
+                pygame.draw.rect(self.screen, item.color, (col_index * self.scale_factor, row_index * self.scale_factor, self.scale_factor, self.scale_factor))
         
         pygame.display.flip()
         
