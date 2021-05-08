7c7fbbd349c3cdaaeca271491ba67d2d1d53f16f
lila <lsmith2@olin.edu>
2021-05-08 19:51:06 -0400

    Added initial test cases.

--- test_snake_controller.py
+++ test_snake_controller.py
@@ -2,16 +2,38 @@
 This module deals with testing some of the functions in the snake_controller
 module.
 """
-import pytest
-from snake_controller import * #Change this to have functions
+import pytest, sys, pygame
+from snake_controller import SnakePlayer
+from snake_model import GameBoard
 
-FUNCTION_1_CASES = [
-    ("",""),
-    ("",""),
+
+GET_INPUT_CASES = [
+    (pygame.K_DOWN,[1,0]), # Check that down-arrow key changes 
+                           # snake direction to down
+    (pygame.K_UP,[-1,0]), # Check that up-arrow key changes 
+                          # snake direction to up
+    (pygame.K_RIGHT,[0,1]), # Check that right-arrow key changes
+                            # snake direction to right
+    (pygame.K_LEFT,[0,-1]), #Check that left-arrow key changes
+                            # snake direction to left
+    (pygame.K_SPACE,[0,0]), # Check that non-arrow key does not
+                            # change direction                       
 ]
 
-@pytest.mark.parametrize("test_input,expected", FUNCTION_1_CASES)
-def test_function_1(test_input, expected):
+@pytest.mark.parametrize("test_input,expected", GET_INPUT_CASES)
+def test_get_input(test_input, expected):
     """
+    Adds given key press to event queue and checks snake direction is expected.
+
+    Args:
+        test_input: A pygame key
     """
-    #assert function1(test_input) == expected
+    # Set up GameBoard and 
+    pygame.init()
+    board = GameBoard(11)
+    controller = SnakePlayer(board)
+    pygame.event.clear()
+    newevent = pygame.event.Event(pygame.KEYDOWN, key=test_input) #create the event
+    pygame.event.post(newevent) #add the event to the queue
+    controller.get_input()
+    assert board.direction == expected
