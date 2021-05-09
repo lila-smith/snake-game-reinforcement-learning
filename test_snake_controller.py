9055bf4bc04123dd3566fd5f3abf0c3581b3a7f0
lila <lsmith2@olin.edu>
2021-05-08 20:43:46 -0400

    Added unit tests to test_snake_controller.

--- test_snake_controller.py
+++ test_snake_controller.py
@@ -3,7 +3,7 @@ This module deals with testing some of the functions in the snake_controller
 module.
 """
 import pytest, sys, pygame
-from snake_controller import SnakePlayer
+from snake_controller import SnakePlayer, get_restart_input, check_input_list
 from snake_model import GameBoard
 
 
@@ -26,7 +26,8 @@ def test_get_input(test_input, expected):
     Adds given key press to event queue and checks snake direction is expected.
 
     Args:
-        test_input: A pygame key
+        test_input: A pygame key constant.
+        expected: A list of two integers that signify a direction (row, col).
     """
     # Set up GameBoard and 
     pygame.init()
@@ -37,3 +38,53 @@ def test_get_input(test_input, expected):
     pygame.event.post(newevent) #add the event to the queue
     controller.get_input()
     assert board.direction == expected
+
+CHECK_INPUT_LIST_CASES = [
+    ([],False), # Check that empty event queue returns False
+    ([pygame.KEYDOWN],True), # Check that single KEYDOWN event returns True
+    ([pygame.KEYUP],False), # Check that single non-KEYDOWN event returns False
+    ([pygame.KEYDOWN, pygame.KEYUP],True), #Check that two events, with
+                                           # one KEYDOWN first, returns True
+    ([pygame.KEYUP, pygame.KEYDOWN],True), #Check that two events, with
+                                           # one KEYDOWN second, returns True
+    ([pygame.MOUSEWHEEL, pygame.KEYUP],False), #Check that two events, with
+                                               # no KEYDOWN, returns False
+    ([pygame.KEYDOWN, pygame.KEYDOWN],True), #Check that two KEYDOWN events
+                                             # return True
+]
+
+@pytest.mark.parametrize("test_input,expected", CHECK_INPUT_LIST_CASES)
+def test_check_input_list(test_input, expected):
+    pygame.init()
+    board = GameBoard(11)
+    controller = SnakePlayer(board)
+    pygame.event.clear()
+    for event in test_input:
+        newevent = pygame.event.Event(event) #create the event
+        pygame.event.post(newevent) #add the event to the queue
+    assert check_input_list(pygame.KEYDOWN) == expected
+    
+
+
+GET_RESTART_INPUT_CASES = [
+    (pygame.K_y,True), # Check that function returns True when y is pressed
+    (pygame.K_n,False), # Check that function returns False when n is pressed
+    (pygame.K_SPACE,None), # Check that other keys return None
+]
+
+@pytest.mark.parametrize("test_input,expected", GET_RESTART_INPUT_CASES)
+def test_restart_input_cases(test_input, expected):
+    """
+    Simulates a key pressed and checks that get_restart_input is as expected.
+
+    Args:
+        test_input: A pygame key constant.
+        expected: Return from get_restart_input (Boolean or None)
+    """
+    pygame.init()
+    board = GameBoard(11)
+    controller = SnakePlayer(board)
+    pygame.event.clear()
+    newevent = pygame.event.Event(pygame.KEYDOWN, key=test_input) #create the event
+    pygame.event.post(newevent) #add the event to the queue
+    assert get_restart_input() == expected
